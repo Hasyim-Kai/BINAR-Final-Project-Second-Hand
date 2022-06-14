4e0b6be0c5e048fc3f751795cc0d874c518fa8c3
ichihatsu1 <ichihatsu1@gmail.com>
2022-06-14 14:28:12 +0700

    Feat: Add Profile Page

---
+++ pages/ProfilePage.jsx
@@ -0,0 +1,55 @@
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
+export default function ProfilePage() {
+    const [selectedFile, setSelectedFile] = useState()
+    const [preview, setPreview] = useState("/images/default_profile_photo.png")
+    const inputStyle = `rounded-xl px-3 py-2 border w-full mt-1`
+
+    // create a preview as a side effect, whenever selected file is changed
+    useEffect(() => {
+        if (!selectedFile) { return }
+        let objectUrl = URL.createObjectURL(selectedFile)
+        setPreview(objectUrl)
+        // free memory when ever this component is unmounted
+        return () => URL.revokeObjectURL(objectUrl)
+    }, [selectedFile])
+
+    function handleUploadImages(event) { setSelectedFile(event.target.files[0]) }
+
+    return <div className="min-h-screen max-w-4xl mx-auto relative flex flex-col items-center gap-4 pt-10">
+        <h1 className="mb-5 text-xl">Lengkapi Info Akun</h1>
+        <label htmlFor="profilePhoto" className="cursor-pointer">
+            <div className="overflow-hidden w-28 h-28 rounded-xl hover:scale-110 transition-all duration-300">
+                <img className={`object-cover h-full w-full`} src={preview} alt="cat" />
+            </div>
+            <input onChange={handleUploadImages} className="hidden" type="file" name="profilePhoto" accept="image/*" id="profilePhoto" />
+        </label>
+
+        <form className="w-3/5 mt-10 mb-24">
+            <div className="mb-5">
+                <label>Nama</label><br />
+                <input className={inputStyle} type="text" name="name" placeholder="Nama Lengkap" required />
+            </div>
+
+            <div className="mb-5">
+                <label>Kota</label><br />
+                <input className={inputStyle} type="text" name="kota" placeholder="Kota Anda" required />
+            </div>
+
+            <div className="mb-5">
+                <label>Alamat</label><br />
+                <input className={inputStyle} type="text" name="alamat" placeholder="Alamat Anda" required />
+            </div>
+
+            <div className="mb-8">
+                <label>No Handphone</label><br />
+                <input className={inputStyle} type="number" name="nohp" placeholder="Contoh: 08968888123" pattern="{6,}" required />
+            </div>
+
+            <button className={`h-10 py-2 px-3 text-white bg-primaryPurple rounded-xl w-full`}>Simpan</button>
+        </form>
+
+        <Link className="absolute left-28" to='/'><img src="/icons/fi_arrow-left.svg" alt="arrow left" /></Link>
+    </div>
+}
