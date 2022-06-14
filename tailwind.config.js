3683258491f9eb098421855b529514501fc741bf
Hasyim Kai <ichihatsu1@gmail.com>
2022-06-14 13:08:07 +0700

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primaryPurple: '#7126B5',
+        secondaryPurple: '#E2D4F0'
+      }
+    },
+  },
+  plugins: [],
+}
