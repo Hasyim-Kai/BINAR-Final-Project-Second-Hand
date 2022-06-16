6ca71110aa872abd4a1aee973dd86bb97f125641
egi rizki ramadani <https://gitlab.com/egi321910004/basic_git.git>
2022-06-16 16:10:11 +0700

    Send data to Redux (#4)

--- src/redux/action/authAction.js
+++ src/redux/action/authAction.js
@@ -1,6 +1,12 @@
 export const Login = (data) => {
-    console.log('data telah masuk', data)
-    return (dispatch) => {
-        dispatch({ type: "SET_DATA_LOGIN", payload: data });
-    };
+  console.log("data telah masuk", data);
+  return (dispatch) => {
+    dispatch({ type: "SET_DATA_LOGIN", payload: data });
+  };
+};
+export const Register = (data) => {
+  console.log("data telah masuk", data);
+  return (dispatch) => {
+    dispatch({ type: "SET_DATA_REGISTER", payload: data });
+  };
 };
