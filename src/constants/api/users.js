bd397a828475f78ab00b63016b8647b5414b929a
egi rizki ramadani <https://gitlab.com/egi321910004/basic_git.git>
2022-06-23 20:40:19 +0700

    integration API (#11)

--- src/constants/api/users.js
+++ src/constants/api/users.js
@@ -1,5 +1,14 @@
 import axios from "axios";
 
 export default {
-    login: (credentials) => axios.post("https://second-hand-kelompok-5.herokuapp.com/api/login", credentials)
-}
+  login: (credentials) =>
+    axios.post(
+      "https://second-hand-kelompok-5.herokuapp.com/api/login",
+      credentials
+    ),
+  register: (credentials) =>
+    axios.post(
+      "https://second-hand-kelompok-5.herokuapp.com/api/register",
+      credentials
+    ),
+};
--- src/redux/action/authAction.js
+++ src/redux/action/authAction.js
@@ -1,20 +1,20 @@
 import users from "../../constants/api/users";
 
 export const LoginAction = (data) => (dispatch) => {
-    users.login(data).then(res => {
-        console.log('lempar data', res)
-        dispatch({
-            type: "SET_DATA_LOGIN",
-            payload: data
-        });
-    })
+  users.login(data).then((res) => {
+    console.log("lempar data", res);
+    dispatch({
+      type: "SET_DATA_LOGIN",
+      payload: data,
+    });
+  });
 };
-export const Register = (data) => {
-    console.log("data telah masuk", data);
-    return (dispatch) => {
-        dispatch({
-            type: "SET_DATA_REGISTER",
-            payload: data
-        });
-    };
+export const Register = (data) => (dispatch) => {
+  users.register(data).then((res) => {
+    console.log("lempar data", res);
+    dispatch({
+      type: "SET_DATA_REGISTER",
+      payload: data,
+    });
+  });
 };
