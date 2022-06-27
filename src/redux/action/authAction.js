c62b319963534808f52b41ed0f07116ece28662e
Diananaa <d1anasite04@gmail.com>
2022-06-27 19:08:48 +0800

    update : change name call api

--- src/redux/action/authAction.js
+++ src/redux/action/authAction.js
@@ -21,19 +21,22 @@ export const LoginAction = (data, navigate, callback) => (dispatch) => {
 };
 
 export const RegisterAction = (data, callback) => (dispatch) => {
-  if (data.password.length > 6) {
-    users.register(data).then((res) => {
-      callback();
-      console.log(res)
-      dispatch({
-        type: "SET_DATA_REGISTER",
-        payload: data
+  if (data.password.length < 6) {
+    usersAPI
+      .register(data)
+      .then((res) => {
+        callback();
+        console.log(res);
+        dispatch({
+          type: "SET_DATA_REGISTER",
+          payload: data,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+        console.log(err.request.response);
+        alert("Register Failed");
       });
-    }).catch((err) => {
-      console.log(err)
-      console.log(err.request.response)
-      alert("Register Failed");
-    });
   } else {
     alert("Password Below 6 Characters");
   }
