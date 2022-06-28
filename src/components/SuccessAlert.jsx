c0904e18951c851869ad5c8a75dafe817aef2e85
egi rizki ramadani <https://gitlab.com/egi321910004/basic_git.git>
2022-06-28 15:09:53 +0700

    Success add product Alert (#13)

---
+++ src/components/SuccessAlert.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+export default function SuccessAlert({ showAlert = false, message }) {
+  const [isShow, setIsShow] = useState(showAlert);
+
+  if (showAlert == true) {
+    setTimeout(() => {
+      setIsShow(false);
+    }, 2000);
+    // clearTimeout(timer);
+  }
+
+  return isShow ? (
+    <div
+      className="absolute left-0 right-0 p-4 mb-4 text-white bg-green-600 shadow-xl rounded-lg w-7/12 mx-auto flex justify-between" role="alert"
+      
+    >
+      <p>
+        <b>{message}</b>
+      </p>
+      <button className="" onClick={() => setIsShow(false)}>
+        <b>X</b>
+      </button>
+    </div>
+  ) : (
+    <></>
+  );
+}
