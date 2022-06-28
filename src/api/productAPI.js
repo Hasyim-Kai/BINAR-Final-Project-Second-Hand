eafdecd7a7ad185742f43823350869b7dafd55b6
egi rizki ramadani <https://gitlab.com/egi321910004/basic_git.git>
2022-06-28 15:06:40 +0700

    Integration API product (#13)

---
+++ src/api/productAPI.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const token = localStorage.getItem("user:token");
+let config = { headers: { Authorization: "Bearer " + token } };
+
+export default {
+  getAll: () =>
+    axios.get(
+      "https://second-hand-kelompok-5.herokuapp.com/api/product",
+      config
+    ),
+  PostProduct: () =>
+    axios.post(
+      "https://second-hand-kelompok-5.herokuapp.com/api/product",
+      config
+    ),
+};
