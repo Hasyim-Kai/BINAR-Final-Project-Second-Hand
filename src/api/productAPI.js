4b70916be64edac3bee191396fecb51a84042b33
egi rizki ramadani <https://gitlab.com/egi321910004/basic_git.git>
2022-06-28 15:09:51 +0700

    Success add product Alert (#13)

--- src/api/productAPI.js
+++ src/api/productAPI.js
@@ -2,13 +2,14 @@ import axios from "axios";
 
 const token = localStorage.getItem("user:token");
 let config = { headers: { Authorization: "Bearer " + token } };
-
+//Error
 export default {
   getAll: () =>
     axios.get(
       "https://second-hand-kelompok-5.herokuapp.com/api/product",
       config
     ),
+
   PostProduct: () =>
     axios.post(
       "https://second-hand-kelompok-5.herokuapp.com/api/product",
