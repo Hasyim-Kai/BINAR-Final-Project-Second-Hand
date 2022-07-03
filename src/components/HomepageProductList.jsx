2b71200355ea70fedd87842ad2c91e9e6a252165
Diananaa <d1anasite04@gmail.com>
2022-07-04 00:10:38 +0800

    feat : update loading home page (#16)

--- src/components/HomepageProductList.jsx
+++ src/components/HomepageProductList.jsx
@@ -39,52 +39,53 @@ export default function HomepageProductList() {
 
   return (
     <>
-      <section className="container mx-auto px-2 my-10">
-        {/* CATEGORY BUTTON LIST */}
-        <h1>
-          <b>Telusuri Kategori</b>
-        </h1>
-        {isLoading == true ? (
-          <Loading />
-        ) : (
-          <div className="flex gap-5 mt-3 flex-wrap">
-            {filterBtn.map((item) =>
-              category === item ? (
-                <SearchButton
-                  text={item}
-                  anyFunction={onCategoryChange}
-                  key={item}
-                />
-              ) : (
-                <SecondarySearchButton
-                  text={item}
-                  anyFunction={onCategoryChange}
-                  key={item}
-                />
-              )
-            )}
-          </div>
-        )}
-        {/* <Loading /> */}
-      </section>
+      {isLoading == true ? (
+        <Loading />
+      ) : (
+        <>
+          <section className="container mx-auto px-2 my-10">
+            {/* CATEGORY BUTTON LIST */}
+            <h1>
+              <b>Telusuri Kategori</b>
+            </h1>
+            <div className="flex gap-5 mt-3 flex-wrap">
+              {filterBtn.map((item) =>
+                category === item ? (
+                  <SearchButton
+                    text={item}
+                    anyFunction={onCategoryChange}
+                    key={item}
+                  />
+                ) : (
+                  <SecondarySearchButton
+                    text={item}
+                    anyFunction={onCategoryChange}
+                    key={item}
+                  />
+                )
+              )}
+            </div>
+          </section>
 
-      {/* ITEM LIST */}
-      <section className="container mx-auto px-2 mb-28 mt-10 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 justify-between gap-3">
-        {filteredDataDummy.length < 1 ? (
-          <EmptyProductNotification isOnGrid={true} />
-        ) : (
-          filteredDataDummy.map((item) => (
-            <ProductItem
-              key={item.id}
-              id={item.id}
-              name={item.nama}
-              category={filterBtn[item.kategori_id]}
-              price={item.harga}
-              img={item.img_url}
-            />
-          ))
-        )}
-      </section>
+          {/* ITEM LIST */}
+          <section className="container mx-auto px-2 mb-28 mt-10 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 justify-between gap-3">
+            {filteredDataDummy.length < 1 ? (
+              <EmptyProductNotification isOnGrid={true} />
+            ) : (
+              filteredDataDummy.map((item) => (
+                <ProductItem
+                  key={item.id}
+                  id={item.id}
+                  name={item.nama}
+                  category={filterBtn[item.kategori_id]}
+                  price={item.harga}
+                  img={item.img_url}
+                />
+              ))
+            )}
+          </section>
+        </>
+      )}
     </>
   );
 }
