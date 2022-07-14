3337263647892feb126bc7355fe4b9f43d08763d
ichihatsu1 <ichihatsu1@gmail.com>
2022-07-14 07:26:16 +0700

    Feat: Add Buyer Notification

--- src/components/NotificationDropdown.jsx
+++ src/components/NotificationDropdown.jsx
@@ -1,10 +1,18 @@
 import NotificationItem from "../components/NotificationItem";
+import { useDispatch, useSelector } from "react-redux";
+import { getSellerNotif, getBuyerNotif } from "../redux/action/transactionAction";
 import { useState, useEffect, useRef } from 'react';
 
 const NotificationDropdown = () => {
-    const wrapperRef = useRef(null);
-    const [isDropdownOpen, setDropdownOpen] = useState(false);
-    function handleSetDropdownOpen(event) { setDropdownOpen(!isDropdownOpen); };
+  const dispatch = useDispatch();
+  const { sellerNotification, buyerNotification } = useSelector((state) => state.interestReducer);
+  const wrapperRef = useRef(null);
+  const [isDropdownOpen, setDropdownOpen] = useState(false);
+  function handleSetDropdownOpen(event) { 
+    setDropdownOpen(!isDropdownOpen);
+    dispatch(getSellerNotif());
+    dispatch(getBuyerNotif());
+  };
 
     useEffect(() => {
         function handleClickOutside(event) {
@@ -21,13 +29,26 @@ const NotificationDropdown = () => {
     }, [wrapperRef]);
 
     return <div className="relative" ref={wrapperRef}>
-        <button onClick={handleSetDropdownOpen}><img src="/icons/fi_bell.svg" alt="Notification" /></button>
+        <button onClick={handleSetDropdownOpen}>
+            {sellerNotification.length > 0 ? <img src="/icons/fi_bell_dot.svg" alt="New Notification" />
+            : <img src="/icons/fi_bell.svg" alt="Notification" />}
+        </button>
 
         {/* <!-- dropdown menu --> */}
         <div className={`absolute -right-7 p-5 mt-1 bg-white rounded-xl border shadow-xl ${isDropdownOpen ? 'flex flex-col' : 'hidden'} z-50`}>
             <ul className="lg:w-96 w-72">
-                <NotificationItem isBargained={true} />
-                <NotificationItem />
+                {/* IF NO NOTIF, SHOW EMPTY */}
+                {sellerNotification.length < 1 && buyerNotification.length < 1 && <h1 className="text-center">Currently Empty</h1>}
+                
+                {/* SELLER NOTIFICATION */}
+                {sellerNotification.length < 1 && sellerNotification.map( item => <NotificationItem productName = {item.product.nama}
+                productPrice = {item.product.harga} productBargainedPrice = {item.harga_tawar} dateBargained = {item.createdAt}
+                productImg={item.product.pictures[0].img_url} isBargained={true} key={item.id}/>)}
+
+                {/* BUYER NOTIFICATION */}
+                {buyerNotification.length < 1 && buyerNotification.map( item => <NotificationItem productName = {item.product.nama}
+                productPrice = {item.product.harga} productBargainedPrice = {item.harga_tawar} dateBargained = {item.createdAt}
+                productImg={item.product.pictures[0].img_url} isBargained={true} key={item.id}/>)}
             </ul>
         </div>
     </div>
--- src/services/api/transactionAPI.js
+++ src/services/api/transactionAPI.js
@@ -10,4 +10,40 @@ export default {
       postData,
       config
     ),
+
+  transactionByCurrentUser: () =>
+    axios.get(
+      "https://second-hand-kelompok-5.herokuapp.com/api/transaction/myproduct",
+      config
+    ),
+
+  soldProductByCurrentUser: () =>
+    axios.get(
+      "https://second-hand-kelompok-5.herokuapp.com/api/transaction/myproduct/sold",
+      config
+    ),
+
+  acceptOrRefuseTransaction: (id, isAccept) =>
+    axios.put(
+      `https://second-hand-kelompok-5.herokuapp.com/api/transaction/terima/id/${id}`,
+      isAccept, config
+    ),
+
+  successOrCancelTransaction: (id, isSold) =>
+    axios.put(
+      `https://second-hand-kelompok-5.herokuapp.com/api/transaction/id/${id}`,
+      isSold, config
+    ),
+
+  getSellerNotification: () =>
+    axios.get(
+      "https://second-hand-kelompok-5.herokuapp.com/api/notification/seller",
+      config
+    ),
+
+  getBuyerNotification: () =>
+    axios.get(
+      "https://second-hand-kelompok-5.herokuapp.com/api/notification/buyer",
+      config
+    ),
 };
