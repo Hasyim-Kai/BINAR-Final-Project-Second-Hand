3cff9a6196bdaf033eca5ac5c78c882c2afb5a38
ichihatsu1 <ichihatsu1@gmail.com>
2022-07-21 10:04:19 +0700

    Refactor : Fixing Notification

--- src/components/NotificationDropdown.jsx
+++ src/components/NotificationDropdown.jsx
@@ -28,31 +28,28 @@ const NotificationDropdown = () => {
     };
   }, [wrapperRef]);
 
+  const dotBell = <img src="/icons/fi_bell_dot.svg" alt="New Notification" />
+  const bell = <img src="/icons/fi_bell.svg" alt="Notification" />
+
   return (
     <div className="relative" ref={wrapperRef}>
       <button onClick={handleSetDropdownOpen}>
-        {sellerNotification.length > 0 ? (
-          <img src="/icons/fi_bell_dot.svg" alt="New Notification" />
-        ) : (
-          <img src="/icons/fi_bell.svg" alt="Notification" />
-        )}
+        {(sellerNotification !== 'null' || buyerNotification !== 'null') ? dotBell : bell }
       </button>
 
       {/* <!-- dropdown menu --> */}
-      <div
-        className={`absolute -right-7 p-5 mt-1 bg-white rounded-xl border shadow-xl ${
-          isDropdownOpen ? "flex flex-col" : "hidden"
-        } z-50`}
-      >
+      <div className={`absolute -right-7 p-5 mt-1 bg-white rounded-xl border shadow-xl 
+        ${isDropdownOpen ? "flex flex-col" : "hidden"} z-50`}>
         <ul className="lg:w-96 w-72">
+
           {/* IF NO NOTIF, SHOW EMPTY */}
-          {/* {sellerNotification.length < 1 && buyerNotification.length < 1 && (
+          {Object.keys(sellerNotification).length < 1 && Object.keys(buyerNotification).length < 1 && (
             <h1 className="text-center">Currently Empty</h1>
-          )} */}
+          )}
 
           {/* SELLER NOTIFICATION */}
-          {sellerNotification.length > 0 &&
-            sellerNotification.concat(buyerNotification).map((item) => (
+          {(Object.keys(sellerNotification).length > 0 && (sellerNotification.penawaran_masuk.length > 0 || sellerNotification.produk_terjual.length > 0))
+          && sellerNotification.penawaran_masuk.concat(sellerNotification.produk_terjual).map((item) => (
               <NotificationItem
                 productName={item.product.nama}
                 productPrice={item.product.harga}
@@ -65,8 +62,8 @@ const NotificationDropdown = () => {
             ))}
 
           {/* BUYER NOTIFICATION */}
-          {/* {buyerNotification.length > 0 &&
-            buyerNotification.map((item) => (
+          {(Object.keys(buyerNotification).length > 0 && (buyerNotification.penawaran_diterbitkan.length > 0 || buyerNotification.penawaran_sudah_diterima.length > 0 || buyerNotification.produk_dibeli.length > 0))
+          && buyerNotification.penawaran_diterbitkan.concat(buyerNotification.penawaran_sudah_diterima, buyerNotification.produk_dibeli.length).map((item) => ( 
               <NotificationItem
                 productName={item.product.nama}
                 productPrice={item.product.harga}
@@ -76,7 +73,8 @@ const NotificationDropdown = () => {
                 isBargained={true}
                 key={item.id}
               />
-            ))} */}
+            ))}
+            
         </ul>
       </div>
     </div>
